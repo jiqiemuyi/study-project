17d1cadc10b8c4eed1120bb299168df884abeadc
jia.yang <muyijiqie@gmail.com>
2024-09-02 00:18:53 +0800

    wip: husky test

---
+++ commitlint.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "wip",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+      ],
+    ],
+    "subject-case": [
+      0,
+      "never",
+      ["sentence-case", "start-case", "pascal-case", "upper-case"],
+    ],
+  },
+};
