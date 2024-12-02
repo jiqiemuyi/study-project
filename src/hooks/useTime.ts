cffdee2a5e48601a3ba32a34e72613acef300335
Jia.Yang <jia.yang@duomai.com>
2024-12-02 13:40:44 +0800

    WIP: add description

--- src/hooks/useTime.ts
+++ src/hooks/useTime.ts
@@ -4,9 +4,9 @@ import React from "react";
  * @Author: Jia.Yang
  * @Date: 2024-11-29 14:48:28
  * @LastEditors: Jia.Yang jia.yang@duomai.com
- * @LastEditTime: 2024-11-29 14:48:34
- * @FilePath: \study-project\src\pages\hooks\useTime.ts
- * @Description:
+ * @LastEditTime: 2024-12-02 13:39:24
+ * @FilePath: \study-project\src\hooks\useTime.ts
+ * @Description:时间hook
  */
 function useTime() {
   const [time, setTime] = React.useState(new Date());
